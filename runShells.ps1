ff4eec488883384bfd7bcb07b8a05d2f9b07f843
yvnsraviteja1729 <75905670+yvnsraviteja1729@users.noreply.github.com>
2022-11-17 13:18:03 +0530

    Update runShells.ps1

--- runShells.ps1
+++ runShells.ps1
@@ -1,7 +1,11 @@
 param (
     [Parameter(Mandatory=$true)][string]$ScriptsPath = ""                                        
 )
-
+write-host $ScirptsPath
+if ( "" -eq $ScriptsPath ) {
+    $ScriptsPath = (get-location).Path
+}
+write-host $ScirptsPath
 $files = get-childitem -path $ScriptsPath -name -include *.ps1 | Where-Object {! $_.PSIsContainer }
 foreach( $file in $files ) {
     $PowerShellFile = (Join-Path -Path $PolicyPath -ChildPath $file)
